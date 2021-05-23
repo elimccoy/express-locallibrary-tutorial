179e9e7a52d4e79283238d61160fc61b30e60433
elimccoy <elijmccoy01@gmail.com>
2021-05-22 17:11:16 -0700

    added to app.js

--- app.js
+++ app.js
@@ -16,7 +16,11 @@ app.use(helmet());
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = "mongodb+srv://locallibraryuser:admin@cluster0.pvxsr.mongodb.net/local_library?retryWrites=true&w=majority";
+
+// Set up mongoose connection
+var dev_db_url = "mongodb+srv://locallibraryuser:admin@cluster0.pvxsr.mongodb.net/local_library?retryWrites=true&w=majority";
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
